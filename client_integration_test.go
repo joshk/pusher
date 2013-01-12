00350e8968b04f3f10d6e0af2463fffab4420b6a
Timon Vonk <timonv@gmail.com>
2013-01-12 15:58:28 +0100

    Test multi publish

--- client_integration_test.go
+++ client_integration_test.go
@@ -4,7 +4,6 @@ import (
   "testing"
   "fmt"
   "time"
-  //"github.com/timonv/pusher"
 )
 
 // Integration level tests
@@ -29,3 +28,42 @@ func TestSimplePublish(t *testing.T) {
 	}
 }
 
+func TestMultiPublish(t *testing.T) {
+	workers := 10
+	messageCount := 100
+	messages := make(chan string)
+	done := make(chan bool)
+
+	client := NewClient("34420", "87bdfd3a6320e83b9289", "f25dfe88fb26ebf75139", false)
+
+	for i := 0; i < workers; i++ {
+		go func() {
+			for data := range messages {
+				err := client.Publish(data, "test", "test")
+				if err != nil {
+          t.Errorf("Error: ", err)
+				} else {
+					fmt.Print(".")
+				}
+			}
+		}()
+	}
+
+	go func() {
+		for i := 0; i < messageCount; i++ {
+			messages <- "test"
+		}
+		done <- true
+		close(messages)
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		fmt.Println("\nDone :-)")
+	case <-time.After(1 * time.Minute):
+		fmt.Println("\nTimeout :-(")
+	}
+
+	fmt.Println("")
+}
