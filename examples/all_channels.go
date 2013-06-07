4133eca2165b19c0ec5b78599b7fb788089d5812
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-06-08 00:51:25 +0200

    TABS, BE GONE WITH YOU!

--- examples/all_channels.go
+++ examples/all_channels.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"github.com/timonv/pusher"
-	"sort"
-	"time"
+    "fmt"
+    "github.com/timonv/pusher"
+    "sort"
+    "time"
 )
 
 func main() {
-	client := pusher.NewClient("4115", "23ed642e81512118260e", "cd72de5494540704dcf1", false)
+    client := pusher.NewClient("4115", "23ed642e81512118260e", "cd72de5494540704dcf1", false)
 
-	done := make(chan bool)
+    done := make(chan bool)
 
-	go func() {
-		channels, err := client.AllChannels()
-		if err != nil {
-			fmt.Printf("Error %s\n", err)
-		} else {
-			names := []string{}
-			for k := range channels.List {
-				names = append(names, k)
-			}
-			sort.Strings(names)
-			fmt.Println("Channel Count:", len(names))
-			fmt.Println(names)
-		}
-		done <- true
-	}()
+    go func() {
+        channels, err := client.AllChannels()
+        if err != nil {
+            fmt.Printf("Error %s\n", err)
+        } else {
+            names := []string{}
+            for k := range channels.List {
+                names = append(names, k)
+            }
+            sort.Strings(names)
+            fmt.Println("Channel Count:", len(names))
+            fmt.Println(names)
+        }
+        done <- true
+    }()
 
-	select {
-	case <-done:
-		fmt.Println("Done :-)")
-	case <-time.After(1 * time.Minute):
-		fmt.Println("Timeout :-(")
-	}
+    select {
+    case <-done:
+        fmt.Println("Done :-)")
+    case <-time.After(1 * time.Minute):
+        fmt.Println("Timeout :-(")
+    }
 }
