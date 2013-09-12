6072865e0259074babf8d03d25832f665d872cf5
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-09-12 23:59:49 +0200

    use the default go coding standards

--- signature.go
+++ signature.go
@@ -1,26 +1,26 @@
 package pusher
 
 import (
-    "crypto/hmac"
-    "crypto/md5"
-    "crypto/sha256"
-    "encoding/hex"
-    "fmt"
-    "io"
-    "net/url"
-    "sort"
-    "strings"
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"net/url"
+	"sort"
+	"strings"
 )
 
 type Signature struct {
-    key, secret                          string
-    method, path, timestamp, authVersion string
-    content                              []byte
-    queryParameters                      map[string]string
+	key, secret                          string
+	method, path, timestamp, authVersion string
+	content                              []byte
+	queryParameters                      map[string]string
 }
 
 type AuthPart struct {
-    key, value string
+	key, value string
 }
 
 type OrderedAuthParts []*AuthPart
@@ -30,73 +30,73 @@ func (s OrderedAuthParts) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s OrderedAuthParts) Less(i, j int) bool { return s[i].key < s[j].key }
 
 func (s *Signature) Sign() string {
-    authParts := []*AuthPart{
-        {"auth_key", s.key},
-        {"auth_timestamp", s.timestamp},
-        {"auth_version", s.authVersion},
-        {"body_md5", s.md5Content()},
-    }
+	authParts := []*AuthPart{
+		{"auth_key", s.key},
+		{"auth_timestamp", s.timestamp},
+		{"auth_version", s.authVersion},
+		{"body_md5", s.md5Content()},
+	}
 
-    for k := range s.queryParameters {
-        authParts = append(authParts, &AuthPart{k, s.queryParameters[k]})
-    }
+	for k := range s.queryParameters {
+		authParts = append(authParts, &AuthPart{k, s.queryParameters[k]})
+	}
 
-    sort.Sort(OrderedAuthParts(authParts))
+	sort.Sort(OrderedAuthParts(authParts))
 
-    sortedAuthParts := []string{}
-    for index := range authParts {
-        newPart := fmt.Sprintf("%s=%s", authParts[index].key, authParts[index].value)
-        sortedAuthParts = append(sortedAuthParts, newPart)
-    }
+	sortedAuthParts := []string{}
+	for index := range authParts {
+		newPart := fmt.Sprintf("%s=%s", authParts[index].key, authParts[index].value)
+		sortedAuthParts = append(sortedAuthParts, newPart)
+	}
 
-    authPartsQueryString := strings.Join(sortedAuthParts, "&")
-    completeAuthParts := fmt.Sprintf("%s\n%s\n%s", s.method, s.path, authPartsQueryString)
+	authPartsQueryString := strings.Join(sortedAuthParts, "&")
+	completeAuthParts := fmt.Sprintf("%s\n%s\n%s", s.method, s.path, authPartsQueryString)
 
-    return s.hmacSha256(completeAuthParts)
+	return s.hmacSha256(completeAuthParts)
 }
 
 func (s *Signature) EncodedQuery() string {
-    query := url.Values{
-        "auth_key":       {s.key},
-        "auth_timestamp": {s.timestamp},
-        "auth_version":   {s.authVersion},
-        "body_md5":       {s.md5Content()},
-        "auth_signature": {s.Sign()},
-    }
-    for k := range s.queryParameters {
-        query.Add(k, s.queryParameters[k])
-    }
-    return query.Encode()
+	query := url.Values{
+		"auth_key":       {s.key},
+		"auth_timestamp": {s.timestamp},
+		"auth_version":   {s.authVersion},
+		"body_md5":       {s.md5Content()},
+		"auth_signature": {s.Sign()},
+	}
+	for k := range s.queryParameters {
+		query.Add(k, s.queryParameters[k])
+	}
+	return query.Encode()
 }
 
 func (s *Signature) auth_key() string {
-    return "auth_key=" + s.key
+	return "auth_key=" + s.key
 }
 
 func (s *Signature) auth_timestamp() string {
-    return "auth_timestamp=" + s.timestamp
+	return "auth_timestamp=" + s.timestamp
 }
 
 func (s *Signature) auth_version() string {
-    return "auth_version=" + s.authVersion
+	return "auth_version=" + s.authVersion
 }
 
 func (s *Signature) body_md5() string {
-    return "body_md5=" + s.md5Content()
+	return "body_md5=" + s.md5Content()
 }
 
 func (s *Signature) md5Content() string {
-    return s.md5(s.content)
+	return s.md5(s.content)
 }
 
 func (s *Signature) md5(content []byte) string {
-    hash := md5.New()
-    hash.Write(content)
-    return hex.EncodeToString(hash.Sum(nil))
+	hash := md5.New()
+	hash.Write(content)
+	return hex.EncodeToString(hash.Sum(nil))
 }
 
 func (s *Signature) hmacSha256(content string) string {
-    hash := hmac.New(sha256.New, []byte(s.secret))
-    io.WriteString(hash, content)
-    return hex.EncodeToString(hash.Sum(nil))
+	hash := hmac.New(sha256.New, []byte(s.secret))
+	io.WriteString(hash, content)
+	return hex.EncodeToString(hash.Sum(nil))
 }
